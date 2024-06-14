bc84fc488680c0a5c9e23c77b42cb6940afcd4c8
yazgoo <yazgoo@gmail.com>
2024-06-14 08:33:44 +0200

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 set -x
-sbt "githubGenerateSnapshot;githubAnalyzeDependencies alerts;githubAnalyzeDependencies cves;githubAnalyzeDependencies list jackson-databind;githubAnalyzeDependencies get jackson-databind"
+sbt "githubGenerateSnapshot;githubAnalyzeDependencies alerts;githubAnalyzeDependencies cves;githubAnalyzeDependencies list jackson-databind;githubAnalyzeDependencies get jackson-databind:2.12.3"
